2da7bcd32ade3e2f5535a0cb92716cbc657e1863
MrityunjayaSahu <sahu.mrityunjaya@clarivate.com>
2020-11-08 20:56:34 +0530

    User1 has Created a DemoClass1.java File

---
+++ src/test/java/com/diapi/automation/customfield/DemoClass1.java
@@ -0,0 +1,12 @@
+package com.diapi.automation.customfield;
+
+import org.testng.annotations.Test;
+
+public class DemoClass1 {
+	
+	@Test
+	public void getText() {
+		System.out.println("getText");
+	}
+
+}
